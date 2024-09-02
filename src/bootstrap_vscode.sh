6cb9a8184081ad3f7248a69fadecc927bc7c0c4d
Frey Bloch Clante <fbc@dsb.dk>
2024-09-02 09:33:02 +0200

    ADD devcontainers - WIP

---
+++ src/bootstrap_vscode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 -m pip install --upgrade build \
+&& python3 -m pip install twine \
+&& python3 -m pip install poetry \
+&& sudo mkdir -p /workspaces/devcontainers-workshop \
+&& sudo chown -R vscode:vscode /workspaces/devcontainers-workshop \
+&& git config --global --add safe.directory /workspaces/devcontainers-workshop 
+
+echo "Done bootstrapping devcontainer workspace"
